5c31bc645748d4988b0148360a80f6e541482b98
Jox <111201149+JohnAlbechaalany@users.noreply.github.com>
2025-09-29 18:52:06 +0200

    Update Tester.R

--- Tester.R
+++ Tester.R
@@ -2,14 +2,10 @@ devtools::document("spectrotune")
 devtools::install("spectrotune")
 library(spectrotune)
 library(readxl)
-library(spectrotune)
-
-df <- readxl::read_excel("C:/Users/user/Desktop/df25.xlsx", sheet = "Overall")
-str(df)
 
 fit <- st_run(
   data = df,
-  target = "Age",
+  target = "",
   preprocess = c("ABS"),
   preprocess_params = list(SG1 = list(w = 17, p = 2, m = 1)),
   algos = c("PLSR","SVR"),

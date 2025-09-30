93a666d302ed3717e33c7df92123a84f13163e76
JohnAlbechaalany <johnbechaalany@gmail.com>
2025-09-30 12:30:41 +0200

    Update package to v1.0.0: docs, models, plotting, save API

--- Tester.R
+++ Tester.R
@@ -1,5 +1,25 @@
+# 1) Clean load state
+if ("spectrotune" %in% loadedNamespaces()) detach("package:spectrotune", unload=TRUE)
+
+# 2) Regenerate docs and ensure exports
+roxygen2::roxygenise("spectrotune")     # or devtools::document("spectrotune")
+
+# 3) Reinstall and reload
+devtools::install("spectrotune", force=TRUE, dependencies=TRUE)
+library(spectrotune)
+
+# 4) Verify functions are exported
+ls("package:spectrotune")
+getNamespaceExports("spectrotune")
+
+install.packages("roxygen2") 
 devtools::document("spectrotune")
 devtools::install("spectrotune")
+devtools::document("spectrotune")
+help(package = "spectrotune")
+?st_run
+library(spectrotune)
+?st_run   # or press F1 on st_run
 library(spectrotune)
 library(readxl)
 
